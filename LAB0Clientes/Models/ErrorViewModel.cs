42891951cfa34470a51413da0a1bea28728dd88c
Rocket <hugofloca769@gmail.com>
2022-02-02 18:49:36 -0600

    Creacion de la solucion

---
+++ LAB0Clientes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LAB0Clientes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
