78b1829eceb0928de16dba6b173900b927206fa1
SAMER ARANKI MUNOZ <saranki@correo.url.edu.gt>
2022-02-02 18:57:26 -0600

    creacion clase cliente

---
+++ LAB0Clientes/Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace LAB0Clientes.Models
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }   
+        public int Telefono { get; set; }
+        public string Detalles { get; set; }
+    }
+}
--- LAB0Clientes/obj/Release/net6.0/LAB0Clientes.AssemblyInfo.cs
+++ LAB0Clientes/obj/Release/net6.0/LAB0Clientes.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LAB0Clientes")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LAB0Clientes")]
